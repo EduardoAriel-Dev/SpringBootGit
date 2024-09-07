5f7f6758ead899f03a5a7f672fce893ebb18020a
Eduardo <eduardoariel2016@outlook.com>
2024-09-07 17:12:16 -0300

    first commit

---
+++ src/main/java/com/example/springbootgit/SpringBootGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitApplication.class, args);
+	    System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/com/example/springbootgit/SpringBootGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
